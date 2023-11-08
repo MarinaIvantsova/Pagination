1eb3e8a107967164c165114d701b089f2b818ef2
Marina Ivantsova <63035225+MarinaIvantsova@users.noreply.github.com>
2023-11-08 14:34:19 +0700

    window.matchMedia instead tailwind media

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,31 +1,53 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaHome } from "react-icons/fa";
 
 function Menu() {
-  const [small, setSmall] = useState(false);
-  const CollapseExpand = small ? ">" : "<";
+  const State = {
+    COLLAPSED: 'collapsed',
+    EXPANDED: 'expanded'
+  }
+  
+  const [menuState, setMenuState] = useState(State.EXPANDED);
+  const [expandCollapse, setExpandCollapse] = useState(true);
+  const [textHome, setHome] = useState(true);
+  const CollapseExpand = menuState === State.COLLAPSED ? ">" : "<";
+  const widerScreenWidth = window.matchMedia("(min-width: 1024px)");
+
+  const handleResize = () => {
+    if (widerScreenWidth.matches) {
+      setExpandCollapse(true);
+      setMenuState(State.EXPANDED); 
+      setHome(true)
+    } else {
+      setExpandCollapse(false);
+      setHome(false)
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    widerScreenWidth.addEventListener("change", handleResize);
+
+    return () => {
+      widerScreenWidth.removeEventListener("change", handleResize);
+    };
+  }, []); 
+
   return (
     <div className="flex text-white">
       <div
-        className={clsx(
-          "w-[40px]",
-          {
-            "lg:w-[200px]": !small,
-          },
-          "h-screen",
-          "p-2",
-          "bg-red-600"
-        )}
+        className={clsx("w-[40px] h-screen p-2 bg-red-600")}
+        style={widerScreenWidth.matches && menuState === State.EXPANDED ? { width: "200px" } : {}}
       >
-        <button className="hidden lg:block" onClick={() => setSmall(!small)} type="button">
-        {CollapseExpand}
+        <button className={expandCollapse ? "block" : "hidden"} onClick={() => setMenuState(prev => prev === State.COLLAPSED ? State.EXPANDED: State.COLLAPSED)} type="button">
+          {CollapseExpand}
         </button>
         <div className="flex">
-        <FaHome size={20} className="mr-1"/> 
-        <div className="hidden lg:block"> 
-          {!small && 'Home'} 
-        </div>
+          <FaHome size={20} className="mr-1" />
+          <div className={textHome ? "block" : 'hidden'}>
+            {menuState === State.EXPANDED && 'Home'}
+          </div>
         </div>
       </div>
       <div className="flex-grow p-2 bg-blue-600">Content</div>
